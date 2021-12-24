af69862d6660f3bb1e9c41922e620f208b228e86
Dhanush kumar Sivaji <dhanushkumarsivaji@gmail.com>
2021-12-24 23:06:29 +0530

    String reversal - Learned about how to reverse a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,22 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+    return str.split('').reduce((reversed,character)=> character+reversed,'')
+}
+
+reverse('dhanush')
 
 module.exports = reverse;
+
+// 1st solution
+//return str.split('').reverse().join('');
+
+// 2nd solution
+// let reversed = ''
+// for(let character of str){
+//     //take the character and add it to the start of the reversed
+//     reversed = character + reversed
+// }
+// return reversed
