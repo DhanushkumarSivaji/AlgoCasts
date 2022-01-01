9e9c2d762a6d7a65edad480701d3071ca32bdefb
Dhanush kumar Sivaji <dhanushkumarsivaji@gmail.com>
2022-01-01 23:33:35 +0530

    Find The Vowels - Learned how to find number of vowels used in the string

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,28 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0
+}
+
+//g dont stop on the first match iterate all
+//i sensitive (lower case & upper case)
 
 module.exports = vowels;
+
+
+//solution 1
+// function vowels(str) {
+//     let count = 0;
+
+//     const checker = ['a','e','i','o','u']
+
+//     for (let char of str.toLowerCase()){
+//         if(checker.includes(char)){
+//             count++
+//         }
+//     }
+
+//     return count;
+// }
